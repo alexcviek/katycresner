d075ca34f30be95bbcc4dfc547878180eff85124
Alex Cwiek <a.i.cwiek@gmail.com>
2018-01-30 21:32:13 +0000

    emails are sent

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
-const path = require('path');
 
 const app = express();
 
@@ -15,7 +14,7 @@ app.use(express.static(`${__dirname}/public`));
 
 
 //Body parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
@@ -23,4 +22,46 @@ app.listen(3000, () => console.log('o hai port 3000'));
 
 app.get('/', (req,res) => {
   res.render('index');
-} );
+});
+
+app.post('/', (req, res) => {
+  const output = `
+    <h1>O HAI</h1>
+    <p>You have a new contact request</p>
+    <h3>Contact details</h3>
+    <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Type: ${req.body.typeofevent}</li>
+    <li>Date: ${req.body.date}</li>
+    <li>E-mail: ${req.body.email}</li>
+    <li>Phone Number: ${req.body.phonenumber}</li>
+    <li>Other Info: ${req.body.otherinfo}</li>
+    </ul>`;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'MY@EMAIL.COM',
+      pass: 'MYPASS'
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  const mailOptions = {
+    from: '"KatyCresner.com Contact" <nickcresner@gmail.com>',
+    to: 'a.i.cwiek@gmail.com',
+    subject: 'Hello ✔',
+    html: output
+  };
+
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    // res.render('index');
+  });
+});
